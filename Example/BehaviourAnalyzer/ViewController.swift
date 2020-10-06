60225939c9e2deda36b9b8729dcc6b45c3872b8a
Deepika <deepikardb@gmail.con>
2020-10-06 15:56:52 +0530

    Integrated pod into app

--- Example/BehaviourAnalyzer/ViewController.swift
+++ Example/BehaviourAnalyzer/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import BehaviourAnalyzer
 
 class ViewController: UIViewController {
 
@@ -20,5 +21,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didTapButton(_ sender: Any) {
+        AnalyticsLogger.trackEvent(ViewControllerEvents.buttonTap)
+    }
+    
 }
 
+enum ViewControllerEvents: AnalyticsEvent {
+    case buttonTap
+    
+    var name: String {
+        switch self {
+        case .buttonTap:
+            return "Button_Tap"
+        }
+    }
+    
+    var parameters: AnalyticsParameters {
+        switch self {
+        default:
+            return [:]
+        }
+    }
+    
+    var userProperties: AnalyticsUserProperties {
+        switch self {
+        default:
+            return [:]
+        }
+    }
+}
